e4fd138cb52f14e4acfac1ab23c773a285029df8
Mehmet Emin Güvercin <105549919+meguvercin@users.noreply.github.com>
2024-09-05 10:34:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let intro = document.querySelector('.intro')
+    const dynamicText = document.querySelector("h1 span");
+    const words = ["MEHMET EMİN GÜVERCİN", "Computer Engineer", "Coding is my...", "PASSION"];
+    let wordIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    const typeEffect = () => {
+      const currentWord = words[wordIndex];
+      const currentChar = currentWord.substring(0, charIndex);
+      dynamicText.textContent = currentChar;
+      dynamicText.classList.add("stop-blinking");
+      if (!isDeleting && charIndex < currentWord.length) {
+          // If condition is true, type the next character
+          charIndex++;
+          setTimeout(typeEffect, 30);
+      } else if (isDeleting && charIndex > 0) {
+          // If condition is true, remove the previous character
+          charIndex--;
+          setTimeout(typeEffect, 30);
+      } else {
+          // If word is deleted then switch to the next word
+          isDeleting = !isDeleting;
+          dynamicText.classList.remove("stop-blinking");
+          wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+          setTimeout(typeEffect, 1200);
+      }
+    }
+    setTimeout(() => {
+      intro.style.top= '-100vh'
+    },12300);
+  typeEffect();
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
